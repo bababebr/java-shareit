2cd1154fb02ce2b076c3177051eb318db69bc077
barabebr <unior1234@yandex.ru>
2023-06-20 00:17:17 +0300

    fix: Item tests

--- src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -9,22 +9,27 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.practicum.shareit.booking.BookingRepository;
 import ru.practicum.shareit.exception.ExceptionsHandler;
+import ru.practicum.shareit.exception.NoSuchObjectException;
+import ru.practicum.shareit.item.comment.CommentRepository;
+import ru.practicum.shareit.item.dto.ItemBookingHistoryDto;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.model.ItemMapper;
-import ru.practicum.shareit.item.model.ItemRepository;
-import ru.practicum.shareit.item.model.ItemService;
+import ru.practicum.shareit.item.model.*;
+import ru.practicum.shareit.request.RequestRepository;
 import ru.practicum.shareit.user.*;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,10 +38,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ItemControllerTest {
     @Mock
     private ItemService itemService;
-    @Mock
-    private ItemRepository itemRepository;
-    @Mock
-    private UserRepository userRepository;
     @InjectMocks
     private ItemController controller;
     private final ObjectMapper mapper = new ObjectMapper();
@@ -45,6 +46,9 @@ class ItemControllerTest {
     private ItemDto itemDto1True;
     private ItemDto itemDto2False;
     private ItemDto itemDto3True;
+
+    private Item item1;
+    private Item item2;
     private UserDto userDto1 = UserDto.create(1L, "user 1", "user 1");
     private UserDto userDto2 = UserDto.create(2L, "user 2", "user 2");
     private User user1 = User.create(1L, "user 1", "user 1");
@@ -52,6 +56,7 @@ class ItemControllerTest {
 
     @BeforeEach
     void setUp() {
+
         mvc = MockMvcBuilders
                 .standaloneSetup(controller)
                 .setControllerAdvice(ExceptionsHandler.class)
@@ -59,16 +64,17 @@ class ItemControllerTest {
         itemDto1True = ItemDto.create(1L, "item 1", "item 1", true, null);
         itemDto2False = ItemDto.create(2L, "item 2", "item 2", false, null);
         itemDto3True = ItemDto.create(3L, "item 3", "item 3", true, null);
+
+        item1 = Item.create(1L, user1, true, itemDto1True.getDescription(),
+                itemDto1True.getName(), itemDto1True.getRequestId());
+        item2 = Item.create(itemDto2False.getId(), user2, itemDto2False.getAvailable(),
+                itemDto2False.getDescription(), itemDto2False.getName(), itemDto2False.getRequestId());
     }
 
     @Test
     void add() throws Exception {
         when(itemService.addItem(any(ItemDto.class), anyLong()))
                 .thenReturn(itemDto1True);
-        when(userRepository.findById(anyLong()))
-                .thenReturn(Optional.ofNullable(user1));
-        when(itemRepository.save(any(Item.class)))
-                .thenReturn(ItemMapper.dtoToItem(itemDto1True, user1));
         mvc.perform(post("/items")
                         .header("X-Sharer-User-Id", userDto1.getId())
                         .content(mapper.writeValueAsString(itemDto1True))
@@ -84,15 +90,72 @@ class ItemControllerTest {
     }
 
     @Test
-    void update() {
+    void addUserNotFound() throws Exception {
+        when(itemService.addItem(any(ItemDto.class), anyLong()))
+                .thenThrow(new NoSuchObjectException("There is no User with ID=1L."));
+        mvc.perform(post("/items")
+                        .header("X-Sharer-User-Id", userDto1.getId())
+                        .content(mapper.writeValueAsString(itemDto1True))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(404));
     }
 
     @Test
-    void getUsersOwnItems() {
+    void update() throws Exception {
+        when(itemService.updateItem(any(ItemDto.class), anyLong(), anyLong()))
+                .thenReturn(itemDto2False);
+        mvc.perform(patch("/items/{itemId}", itemDto2False.getId())
+                        .header("X-Sharer-User-Id", userDto2.getId())
+                        .content(mapper.writeValueAsString(itemDto2False))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(itemDto2False.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(itemDto2False.getName()), String.class))
+                .andExpect(jsonPath("$.description", is(itemDto2False.getDescription()), String.class))
+                .andExpect(jsonPath("$.available", is(itemDto2False.getAvailable()), Boolean.class))
+                .andExpect(jsonPath("$.requestId").doesNotExist());
     }
 
     @Test
-    void get() {
+    void getUsersOwnItems() throws Exception {
+        when(itemService.getUsersOwnItems(anyLong()))
+                .thenReturn(List.of(ItemBookingHistoryDto.create(1L, item1.getName(), item1.getDescription(),
+                        item1.getAvailable(), null, null, null, null)));
+        mvc.perform(MockMvcRequestBuilders.get("/items/")
+                        .header("X-Sharer-User-Id", userDto1.getId())
+                        .content(mapper.writeValueAsString(itemDto1True))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", is(1)));
+    }
+
+    @Test
+    void get() throws Exception {
+        ItemBookingHistoryDto dto = ItemBookingHistoryDto.create(1L, item1.getName(), item1.getDescription(),
+                item1.getAvailable(), null, null, null, null);
+        when(itemService.getItem(anyLong(), anyLong()))
+                .thenReturn(dto);
+
+        mvc.perform(MockMvcRequestBuilders.get("/items/{itemId}", item1.getId())
+                        .header("X-Sharer-User-Id", userDto1.getId())
+                        .content(mapper.writeValueAsString(dto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(dto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(dto.getName()), String.class))
+                .andExpect(jsonPath("$.description", is(dto.getDescription()), String.class))
+                .andExpect(jsonPath("$.available", is(dto.getAvailable()), Boolean.class))
+                .andExpect(jsonPath("$.requestId").doesNotExist())
+                .andExpect(jsonPath("$.lastBooking").doesNotExist())
+                .andExpect(jsonPath("$.nextBooking").doesNotExist());
     }
 
     @Test
