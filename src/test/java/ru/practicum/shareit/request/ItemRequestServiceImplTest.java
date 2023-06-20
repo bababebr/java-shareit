f8f40f61add8917853e2cf5e1251f2706b057347
barabebr <unior1234@yandex.ru>
2023-06-20 17:05:41 +0800

    fix: Tests coverage

--- src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
@@ -10,8 +10,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.booking.Booking;
-import ru.practicum.shareit.booking.BookingRepository;
-import ru.practicum.shareit.booking.BookingServiceImpl;
 import ru.practicum.shareit.booking.BookingStatus;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.exception.ItemsAvailabilityException;
@@ -23,13 +21,13 @@ import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 
 import javax.transaction.Transactional;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
@@ -50,11 +48,6 @@ class ItemRequestServiceImplTest {
     UserRepository userRepository;
     private ItemRequestDto itemRequestDto;
     private ItemRequest itemRequest;
-    private Booking booking;
-    private Booking bookingCangelled;
-    private Booking bookingApproved;
-    private Booking bookingRejected;
-    private BookingDto bookingDto;
     private Item item;
     private User user;
     private User owner;
@@ -72,11 +65,6 @@ class ItemRequestServiceImplTest {
         startFuture = LocalDateTime.now().plusHours(1);
         startPast = LocalDateTime.now().minusHours(1);
         end = LocalDateTime.now().plusHours(2);
-        booking = Booking.create(1L, item, user, startFuture, end, BookingStatus.WAITING);
-        bookingApproved = Booking.create(2L, item, user, startPast, end, BookingStatus.APPROVED);
-        bookingCangelled = Booking.create(3L, item, user, startPast, end, BookingStatus.CANCELLED);
-        bookingRejected = Booking.create(4L, item, user, startPast, startPast.plusMinutes(3), BookingStatus.REJECTED);
-        bookingDto = BookingDto.create(1L, item.getId(), item, user, startFuture, end, BookingStatus.WAITING);
 
         itemRequestDto = ItemRequestDto.create(1L, "request 1", startFuture, new ArrayList<>(), user.getId());
         itemRequest = ItemRequest.create(1L, "request 1", startFuture, user.getId(), item.getId());
@@ -259,11 +247,21 @@ class ItemRequestServiceImplTest {
     }
 
     @Test
-    void getOther() {
+    void getOtherIsNull() {
         when(requestRepository.findAllForOtherUsers(anyLong()))
                 .thenReturn(List.of(itemRequest));
         itemRequest.setItemId(null);
         List<ItemRequestDto> itemRequestDtos = requestService.getOtherRequest(user.getId(), 0, 10);
         assertEquals(itemRequestDtos.get(0).getItems().isEmpty(), true);
+    }
+
+    @Test
+    void getOther() {
+        when(requestRepository.findAllForOtherUsers(anyLong()))
+                .thenReturn(List.of(itemRequest));
+        when(itemRepository.findById(anyLong()))
+                .thenReturn(Optional.ofNullable(item));
+        List<ItemRequestDto> itemRequestDtos = requestService.getOtherRequest(user.getId(), 0, 10);
+        assertEquals(itemRequestDtos.get(0).getItems().size(), 1);
     }
 }
