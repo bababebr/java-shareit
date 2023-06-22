39b2afc0839f9a0d79c4b9cee2caab0c30866f91
barabebr <unior1234@yandex.ru>
2023-06-22 09:55:42 +0800

    wip: pageable

--- src/main/java/ru/practicum/shareit/request/RequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/RequestRepository.java
@@ -2,11 +2,12 @@ package ru.practicum.shareit.request;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface RequestRepository extends JpaRepository<ItemRequest, Long> {
 
-    List<ItemRequest> findAllByRequesterId(Long userId);
+    List<ItemRequest> findAllByRequesterId(Long userId, Pageable pageable);
 
     List<ItemRequest> findAllByRequesterIdIsNot(Long userId);
 
