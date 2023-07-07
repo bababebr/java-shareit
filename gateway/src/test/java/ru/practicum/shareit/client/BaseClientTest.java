18dd7ae8fbc11ffd2cdc74786f87128e741384b3
barabebr <unior1234@yandex.ru>
2023-07-07 10:53:31 +0600

    feat: multi module

---
+++ gateway/src/test/java/ru/practicum/shareit/client/BaseClientTest.java
@@ -0,0 +1,141 @@
+package ru.practicum.shareit.client;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.lang.Nullable;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Transactional
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class BaseClientTest {
+
+    @InjectMocks
+    BaseClient client;
+    @Mock
+    RestTemplate rest;
+
+    @BeforeEach
+    void setUp() {
+        client = new BaseClient(rest);
+    }
+
+    @Test
+    void get() {
+        ResponseEntity<Object> response = new ResponseEntity<>("123", HttpStatus.ACCEPTED);
+        Map<String, Object> map = new HashMap<>();
+        Mockito.when(client.get("123", 1L, map)).thenAnswer(invocationOnMock -> {
+            return response;
+        });
+
+    }
+
+    @Test
+    void testGet() {
+    }
+
+    @Test
+    void testGet1() {
+    }
+
+    @Test
+    void post() {
+    }
+
+    @Test
+    void testPost() {
+    }
+
+    @Test
+    void testPost1() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void testPut() {
+    }
+
+    @Test
+    void patch() {
+    }
+
+    @Test
+    void testPatch() {
+    }
+
+    @Test
+    void testPatch1() {
+    }
+
+    @Test
+    void testPatch2() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void testDelete() {
+    }
+
+    @Test
+    void testDelete1() {
+    }
+
+    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, Long userId, @Nullable Map<String, Object> parameters, @Nullable T body) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders(userId));
+
+        ResponseEntity<Object> shareitServerResponse;
+        try {
+            if (parameters != null) {
+                shareitServerResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
+            } else {
+                shareitServerResponse = rest.exchange(path, method, requestEntity, Object.class);
+            }
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
+        }
+        return prepareGatewayResponse(shareitServerResponse);
+    }
+
+    private HttpHeaders defaultHeaders(Long userId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        if (userId != null) {
+            headers.set("X-Sharer-User-Id", String.valueOf(userId));
+        }
+        return headers;
+    }
+
+    private static ResponseEntity<Object> prepareGatewayResponse(ResponseEntity<Object> response) {
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response;
+        }
+
+        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
+
+        if (response.hasBody()) {
+            return responseBuilder.body(response.getBody());
+        }
+
+        return responseBuilder.build();
+    }
+}
